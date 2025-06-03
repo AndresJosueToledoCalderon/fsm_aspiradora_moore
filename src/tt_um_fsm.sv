dab48cb7774bcf89796fcfb96b7e1cd11317faeb
Andrest <80123619+AndresJosueToledoCalderon@users.noreply.github.com>
2025-06-02 18:41:44 -0600

    Update tt_um_fsm.sv

--- src/tt_um_fsm.sv
+++ src/tt_um_fsm.sv
@@ -40,7 +40,7 @@ module tt_um_fsm (
     assign uio_out = 8'b0;
     assign uio_oe = 8'b0;
     assign uio_in = 8'b0;
-    assign ena = 1'b0:
+    assign ena = 1'b0;
     assign rst_n = 1'b0;
     
 endmodule
