c132036b76ac85f772c08f3260c49a07ad17b095
Andrest <80123619+AndresJosueToledoCalderon@users.noreply.github.com>
2025-06-02 19:21:08 -0600

    Update tt_um_fsm.sv

--- src/tt_um_fsm.sv
+++ src/tt_um_fsm.sv
@@ -3,9 +3,9 @@
 module tt_um_fsm (
 
   input wire clk,
-  input wire rst_n,
-  input wire ena,
-  input wire [7:0] ui_in,
+  input logic rst_n,
+  input logic ena,
+  input logic [7:0] ui_in,
   input wire [7:0] uio_in,
   output wire [7:0] uo_out,
   output wire [7:0] uio_out,
@@ -35,9 +35,12 @@ module tt_um_fsm (
     );
 
     // conectamos los leds 0 y 1 con el estado en que se encuentran
-    assign led = uo_out[1:0];
+    assign led = state;
+    assign uo_out[1:0] = led;
     assign uo_out[7:3] = 5'b0;
     assign uio_out = 8'b0;
-    assign uio_oe = 8'b0;
+    assign uio_oe  = 8'b0;
+    assign rst_n  = 1'b0;
+    assign ena  = 1'b0;
         
 endmodule
